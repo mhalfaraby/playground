c5e132f3871211b24a8432f795bf12e42806daae
mhalfaraby <mhalfaraby@gmail.com>
2020-10-22 16:56:25 +0700

    belajar edabit #1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,9 +1,40 @@
+//func nextEdge(_ side1: Int, _ side2: Int) -> Int {
+//    let hasil = ( side1 + side2 ) - 1
+//    return hasil
+//}
+//
+//nextEdge(10, 5)
 
-import UIKit
-let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4, "shark": 0]
-for (animalName, legCount) in numberOfLegs {
-    print("\(animalName)s have \(legCount) legs")
-}
+//func squared(_ b: Int) -> Int {
+//    return b * b
+//}
+//
+//squaed(5)
 
-print("hello")
-print("wowkeren")
+//func convert(_ hours: Int, _ minutes: Int) -> Int {
+//    let convertion = (hours * 3600 ) + (minutes * 60)
+//    return convertion
+//
+//}
+//
+//convert(1, 3)
+
+//
+//func dividesEvenly(_ a: Int, _ b:Int) -> Bool {
+//    let hasil = a % b
+//    if hasil == 0 {
+//        return true
+//    }
+//    else {
+//
+//    }
+//    return false
+//}
+//
+//dividesEvenly(10, 2)
+
+//func dividesEvenly(_ a: Int, _ b:Int) -> Bool {
+//    return a % b == 0
+//}
+//
+//dividesEvenly(10, 2)
