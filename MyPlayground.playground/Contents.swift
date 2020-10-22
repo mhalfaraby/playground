8a912d346a8eda4051b922ee87a05482e9d5d19b
mhalfaraby <mhalfaraby@gmail.com>
2020-10-22 11:58:20 +0700

    xcode commit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,3 +4,5 @@ let numberOfLegs = ["spider": 8, "ant": 6, "cat": 4, "shark": 0]
 for (animalName, legCount) in numberOfLegs {
     print("\(animalName)s have \(legCount) legs")
 }
+
+print("hello world")
