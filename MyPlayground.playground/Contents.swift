a204d0dba2f4ddeb28a7259bdc299bfd22564f3c
mhalfaraby <mhalfaraby@gmail.com>
2020-10-25 17:53:28 +0700

    codeacademy

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,40 +1,4 @@
-//func nextEdge(_ side1: Int, _ side2: Int) -> Int {
-//    let hasil = ( side1 + side2 ) - 1
-//    return hasil
-//}
-//
-//nextEdge(10, 5)
-
-//func squared(_ b: Int) -> Int {
-//    return b * b
-//}
-//
-//squaed(5)
-
-//func convert(_ hours: Int, _ minutes: Int) -> Int {
-//    let convertion = (hours * 3600 ) + (minutes * 60)
-//    return convertion
-//
-//}
-//
-//convert(1, 3)
-
-//
-//func dividesEvenly(_ a: Int, _ b:Int) -> Bool {
-//    let hasil = a % b
-//    if hasil == 0 {
-//        return true
-//    }
-//    else {
-//
-//    }
-//    return false
-//}
-//
-//dividesEvenly(10, 2)
-
-//func dividesEvenly(_ a: Int, _ b:Int) -> Bool {
-//    return a % b == 0
-//}
-//
-//dividesEvenly(10, 2)
+// Write your code below 🎫
+func bookingTicket(passengerName:String, seatClass: String = "Economy",  timeOfDeparture: Int) -> String {
+  return ("\(passengerName), your seat class is \(seatClass), and you will be departing at \(timeOfDeparture).")
+}
