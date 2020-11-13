519c7d80c4df506799f05b8fec719b3bc00cb2de
mhalfaraby <mhalfaraby@gmail.com>
2020-11-13 15:06:49 +0700

    json stuff

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,31 +1,38 @@
+
+
+
+
+
+
 import Foundation
 
-let headers = [
-  "key": "10b472cf0acb29938c934c80f7ac5323",
-  "content-type": "application/x-www-form-urlencoded"
-]
-
-let postData = NSMutableData(data: "origin=501".data(using: String.Encoding.utf8)!)
-postData.append("&destination=114".data(using: String.Encoding.utf8)!)
-postData.append("&weight=1700".data(using: String.Encoding.utf8)!)
-postData.append("&courier=jne".data(using: String.Encoding.utf8)!)
-
-let request = NSMutableURLRequest(url: NSURL(string: "https://api.rajaongkir.com/starter/cost")! as URL,
-                                        cachePolicy: .useProtocolCachePolicy,
-                                    timeoutInterval: 10.0)
-request.httpMethod = "POST"
-request.allHTTPHeaderFields = headers
-request.httpBody = postData as Data
-
-let session = URLSession.shared
-let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-  if (error != nil) {
-    print(error!)
-  }; if let safeData = data {
-         let dataString = String(data: safeData, encoding: .utf8)
-           print(dataString!)
-  }
+struct array {
+    
+    let city_name: String
+    let city_id: String
+    
 }
-)
 
-dataTask.resume()
+
+var new = [(city_name: "Buleleng", city_id: "94"), (city_name: "Denpasar", city_id: "114"), (city_name: "Gianyar", city_id: "128")]
+
+
+var kosong = [String]()
+
+
+new.forEach {(new) in
+ 
+    kosong.append(new.city_id)
+    kosong.append(new.city_name)
+    
+}
+
+print(kosong)
+
+
+
+
+
+
+
+
--- dummyplaygroundiOS/dummyplaygroundiOS/ViewController.swift
+++ dummyplaygroundiOS/dummyplaygroundiOS/ViewController.swift
@@ -9,16 +9,96 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
+    var data: both?
+
+    @IBOutlet weak var picker: UIPickerView!
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        
-        print("hello world")
-        // Do any additional setup after loading the view.
-        
+        picker.dataSource = self
+        picker.delegate = self
     }
-    
 
+
+   
+      
+    @IBAction func loadData(_ sender: UIButton)
+    {
+        let jsonString = """
+        {
+            "reason_list": [
+                {"id": 144, "reason": "ABCD"},
+                {"id": 221, "reason": "WXYZ"}
+            ]
+        }
+        """
+
+        if let data = jsonString.data(using: .utf8)
+        {
+            self.data = try? JSONDecoder().decode(both.self, from: data)
+             DispatchQueue.main.async {[weak self] in
+                   self?.picker.reloadComponent(0) //HERE..!!!
+             }
+        }
+    }
+
+    struct both :Codable{
+       let reason_list : [reasonlist]?
+       let whom_meet : [nameList]?
+    }
+
+    struct reasonlist :Codable{
+      let id: Int?    //This "id" i couldnt access.
+      let reason : String?
+      enum CodingKeys: String, CodingKey {
+
+      case reason
+      case id
+      }
+    }
+
+    struct nameList :Codable{
+        let name : String?
+        enum CodingKeys: String, CodingKey{
+            case name
+        }
+    }
 
 }
 
+
+extension ViewController: UIPickerViewDataSource {
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return self.data?.reason_list?.count ?? 0
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if row >= self.data!.reason_list!.count {
+              return
+          }
+
+        let coba = self.data?.reason_list?[row].id
+
+        
+        print(coba!)
+    }
+
+    
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return self.data?.reason_list?[row].reason
+    }
+    
+}
