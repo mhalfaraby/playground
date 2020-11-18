6d37cdb01ff58913647f39ae86c04d97e1c23d4b
mhalfaraby <mhalfaraby@gmail.com>
2020-11-19 00:15:09 +0700

    update

--- dummyplaygroundiOS/dummyplaygroundiOS/ViewController.swift
+++ dummyplaygroundiOS/dummyplaygroundiOS/ViewController.swift
@@ -7,48 +7,14 @@
 
 import UIKit
 
+    
 class ViewController: UIViewController {
     
- 
-
-   
-    @IBOutlet weak var tes: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//        tes.delegate = self
-//        tes.dataSource = self
-//        
-//    }
-
-
-
 }
 
-//extension ViewController: UITableViewDataSource {
-//    // jumlah table berdasarkan array struct
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // berapa jumlah tablenya
-//        return 3
-//    }
-//    // menampilkan cell nya sesuai array struct
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//
-//        cell.textLabel?.text = "hello world"
-//        return cell
-//}
-//
-//
-//}
-//
-//
-//extension ViewController: UITableViewDelegate {
-//
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print("tapped")
-//    }
-//
-//}
-//
+ 
